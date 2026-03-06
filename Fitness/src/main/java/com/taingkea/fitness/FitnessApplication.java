38c00c72a8c160117a2cb7aaa9c08efb431f8ddb
Taingkea <staingkea@gmail.com>
2026-03-06 23:19:17 +0700

    change

--- Fitness/src/main/java/com/taingkea/fitness/FitnessApplication.java
+++ Fitness/src/main/java/com/taingkea/fitness/FitnessApplication.java
@@ -1,4 +1,11 @@
 package com.taingkea.fitness;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FitnessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessApplication.class, args);
+    }
 }
